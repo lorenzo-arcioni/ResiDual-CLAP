20f425273a4ca63b2cbe7982ff2cd479942dab89
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2026-04-17 21:40:48 +0200

    ok

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,6 @@ from .CLAPWrapper import CLAPWrapper as CLAP
 from .models.residual_clap import ResidualCLAP
 from .datasets.esc50 import ESC50
 from .datasets.tinysol import TinySOL
+from .datasets.vocalsound import VocalSound
+from .datasets.irmas import IRMAS
 from .models.residual_clap_utils import *
